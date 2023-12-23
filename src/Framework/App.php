ca9ecbb73a733965876d3e62fe128e31018f50ba
Nanobolide <maidougeoffroyjaphet@gmail.com>
2023-12-23 01:56:02 +0000

    structure end

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -1,37 +1,31 @@
-<?php 
- 
-    namespace Framework;
+<?php
+
+namespace Framework;
 
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
+class App
+{
+    public function run(ServerRequestInterface $request): ResponseInterface
+    {
+        $uri = $request->getUri()->getPath();
 
-    class App {
+        if (!empty($uri) && $uri[-1] === "/") {
+            $response = (new Response())
+                ->withStatus(301)
+                ->withHeader('Location', substr($uri, 0, -1));
 
-        public function run(ServerRequestInterface $request):ResponseInterface {
-          
-            //Le code app est celui dont les test use 
-            //Les tests mise en place a la struture de l'app
-            //pour faire des test unitaire il faut creer Une ClasseMere qui est sera appeler par notre ClasseMereTest
-            #
-            $uri = $request->getUri()->getPath();
-
-                if (!empty($uri) && $uri[-1] === "/") {
-                    $response = (new Response())
-                        ->withStatus(301)
-                        ->withHeader('Location',substr($uri,0,-1));
-                     
-                    return $response;
-                }
-                if ($uri === '/blog') {
-                    return new Response(200 , [], '<h1>Bienvenue sur le blog</h1>');
-                }
-
-            
-            $response  = new Response(404, [],'<h1>Erreur 404 !</h1>');
-                return $response;
+            return $response;
         }
 
+        if ($uri === '/blog') {
+            return new Response(200, [], '<h1>Bienvenue sur le blog</h1>');
+        }
 
+        $response  = new Response(404, [], '<h1>Erreur 404 !</h1>');
+
+        return $response;
     }
+}
